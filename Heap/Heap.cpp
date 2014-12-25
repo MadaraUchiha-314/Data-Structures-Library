e3caeed66b93eb5ae760dd627176e2b3b88b061f
MadaraUchiha-314 <rohithr31@gmail.com>
2014-12-25 11:13:20 +0530

    Added user specific compare

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -11,6 +11,7 @@
 
 #include <iostream>
 #include <utility>
+#include <functional>
 
 template <class RandomAccessIterator>
 void swap (RandomAccessIterator first,int index1,int index2)
@@ -61,6 +62,43 @@ void make_heap (RandomAccessIterator first,RandomAccessIterator last)
 	
 }
 
+
+template <class RandomAccessIterator,typename Compare>
+void make_heap (RandomAccessIterator first,RandomAccessIterator last,Compare comp)
+{
+
+	int num_ele = (last - first) + 1;
+	for (int i=num_ele-1;i>=1;i--)
+	{
+		if (comp (first[i],first[(i-1)/2]))
+		{
+			/*
+			* Swap Function becomes a little complicated here.
+			*/
+			swap (first,i,(i-1)/2);
+
+			int k = i;
+
+			while ( ( (!comp (first[k],first[2*k+1]) && ((2*k+1)<num_ele) ) ) || ( (!comp (first[k],first[2*k+2]) && ((2*k+2)<num_ele) ) ) )
+			{
+				if (!(comp (first[k],first[2*k+1])))
+				{
+					swap (first,k,(2*k + 1));
+					k = 2*k + 1;
+				}
+				else 
+				{
+					swap (first,k,(2*k + 2));
+					k = 2*k + 2;
+				}
+			}
+
+		}
+	}
+	
+	
+}
+
 int main ()
 {
 	int a[7] = {1,5,3,4,7,8,2};
@@ -71,6 +109,18 @@ int main ()
 		std::cout<<a[i]<<"\t";
 	
 	std::cout<<"\n";
+
+
+	int b[7] = {7,6,5,4,3,2,1};
+
+	make_heap (&b[0],&b[6],std::less<int> ());
+
+	for (int i=0;i<7;i++)
+		std::cout<<b[i]<<"\t";
+	
+	std::cout<<"\n";
+
+
 
 	return 0;
 }
